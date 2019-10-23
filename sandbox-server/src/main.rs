8de6f30167bbfa9d0dca2d312b9b6e16d0c82a96
Jan Steffens <jan-steffens@live.com>
2019-10-23 20:53:41 +0200

    added actix web with static files

--- sandbox-server/src/main.rs
+++ sandbox-server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files as fs;
+use actix_web::{middleware, App, HttpServer};
+
+fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .service(
+                // static files
+                fs::Files::new("/", "./wwwroot/").index_file("index.html"),
+            )
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
 }
